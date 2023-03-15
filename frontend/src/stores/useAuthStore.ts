b092f9e6d73575fb7aa4ee46f05408d7ad1c5f17
Matheus Vieira <matheusmavieira@gmail.com>
2023-03-15 20:21:06 -0300

    Sign in user

--- frontend/src/stores/useAuthStore.ts
+++ frontend/src/stores/useAuthStore.ts
@@ -1,29 +1,64 @@
 import create from 'zustand'
 import CryptoJS from 'crypto-js'
+import jwtDecode, { JwtPayload } from 'jwt-decode'
 import { StoreStatus } from '@/types'
 import { api } from '@/libs'
 
 interface State {
   status: StoreStatus
   isAuthenticated: boolean
+  id: string | null
+  name: string | null
+  email: string | null
+  password: string | null
   signup: (user: {
     name: string
     email: string
     password: string
   }) => Promise<void>
+  signin: (user: { email: string; password: string }) => Promise<void>
+}
+
+interface JwtToken extends JwtPayload {
+  id: string
+  name: string
+  email: string
+}
+
+function login({ token, password }: { token: string; password: string }) {
+  api.defaults.headers.common.Authorization = token
+  const decoded = jwtDecode<JwtToken>(token)
+  console.log(decoded)
+  return {
+    isAuthenticated: true,
+    id: decoded?.id,
+    name: decoded?.name,
+    email: decoded?.email,
+    password
+  }
 }
 
 export const useAuthStore = create<State>((set, get) => ({
   status: StoreStatus.IDLE,
   isAuthenticated: false,
+  id: null,
+  name: null,
+  email: null,
+  password: null,
   signup: async ({ name, email, password }) => {
-    const { data } = await api.post('user', {
+    const { data } = await api.post('user/signup', {
       name,
       email,
-      password: CryptoJS.SHA256(password).toString()
+      password: CryptoJS.SHA3(password).toString()
     })
-    console.log(data)
-    set((state) => ({ isAuthenticated: true }))
+    set((state) => login({ token: data.token, password }))
+  },
+  signin: async ({ email, password }) => {
+    const { data } = await api.post('user/signin', {
+      email,
+      password: CryptoJS.SHA3(password).toString()
+    })
+    set((state) => login({ token: data.token, password }))
   }
 }))
 
